336834dc5268940abfb0e69355b3b085c6c7b760
Alexandre <alexlepoupon@gmail.com>
2015-04-22 10:14:47 +0200

    Ajout version 1

---
+++ HackAton/app/src/main/java/com/example/alexandre/hackaton/MainFragment.java
@@ -0,0 +1,37 @@
+package com.example.alexandre.hackaton;
+
+import org.json.JSONObject;
+
+import fr.cobaltians.cobalt.fragments.CobaltFragment;
+
+/**
+ * Created by Alexandre on 17/04/2015.
+ */
+public class MainFragment extends CobaltFragment {
+    @Override
+    protected boolean onUnhandledCallback(String callback, JSONObject data) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected boolean onUnhandledEvent(String event, JSONObject data, String callback) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected void onUnhandledMessage(JSONObject message) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void onPullToRefreshRefreshed() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void onInfiniteScrollRefreshed() {
+        // TODO Auto-generated method stub
+    }
+}
